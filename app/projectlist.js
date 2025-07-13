e0071aa4878ba2474b57b197dc97db8fe773a3d4
Terzo <mihailoterzic035@gmail.com>
2025-07-13 02:51:21 +0200

    test

--- app/projectlist.js
+++ app/projectlist.js
@@ -1,22 +1,22 @@
 export const projectList = [
   {
     title: 'Original Badman',
-    image: '/images/originalbadman.png',
+    image: '/images/originalbadman.PNG',
     link: 'https://originalbadman.vercel.app/',
   },
   {
     title: 'Minja Varsakovic',
-    image: '/images/minjavarsakovic.png',
+    image: '/images/minjavarsakovic.PNG',
     link: 'https://minjavarsakovic.vercel.app/',
   },
   {
     title: 'Wine & Fine',
-   image: '/images/wineandfine.png',
+   image: '/images/wineandfine.PNG',
     link: 'https://wine-and-fine.com/',
   },
   {
     title: 'Enterijer Centar',
-    image: '/images/enterijer.png',
+    image: '/images/enterijer.PNG',
     link: 'https://enterijermilovanovic.vercel.app/',
   },
   // Add more projects as needed...
